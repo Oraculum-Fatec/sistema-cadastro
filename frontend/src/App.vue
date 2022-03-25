a82190a5954a8851c4f188d26541dc23d971a731
Pedro landim <henriquep35036@gmail.com>
2022-03-25 12:56:27 -0300

    Construction of the Rregistration Form

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Cadastro</router-link> |
+    <router-link to="/tabela">Tabela de Usuarios</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ frontend/src/views/TabelaDeUsuarios.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="tabela">
+    <h1>Tabela de Usuarios...</h1>
+  </div>
+</template>
