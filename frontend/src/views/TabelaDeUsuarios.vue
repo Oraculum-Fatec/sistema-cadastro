e762668bc69399feb02331135adfa4f17c32fefe
arthurzuk <arthurzuk@hotmail.com>
2022-03-25 19:33:54 -0300

    Update folder organization

--- frontend/src/views/TabelaDeUsuarios.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="tabela">
-    <h1>Tabela de Usuarios...</h1>
-  </div>
-</template>
---
+++ src/views/TabelaDeUsuarios.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="tabela">
+    <TableView/>
+  </div>
+</template>
+
+<script>
+import TableView from '@/components/TableView.vue'
+
+export default {
+  name: 'TableView',
+  components: {
+    TableView
+  }
+}
+</script>
